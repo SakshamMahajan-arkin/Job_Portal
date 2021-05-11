39f5a881c6e2c0f28483279feb15211f9450a22b
Saksham Mahajan <sakshammahajan441@gmail.com>
2021-05-12 01:11:47 +0530

    Need to fix js in bootstrap

---
+++ nav_bar_candidate/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavbarCandidate from './components/navbar.js'
+import Head from './components/head.js'
+import BootstrapJs from './components/bootstrap_java_script.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Head />
+      <NavbarCandidate/>
+      <BootstrapJs/>
+    </div>
+  );
+}
+
+export default App;
