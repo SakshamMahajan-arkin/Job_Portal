6a71a609c0781202ebe0fd56c75794d793f5f6c8
Saksham Mahajan <sakshammahajan441@gmail.com>
2021-05-13 00:03:18 +0530

    Completed Nav-bar

--- nav_bar_candidate/src/App.js
+++ nav_bar_candidate/src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 import NavbarCandidate from './components/navbar.js'
-import Head from './components/head.js'
-import BootstrapJs from './components/bootstrap_java_script.js'
 
 function App() {
   return (
     <div className="App">
-      <Head />
       <NavbarCandidate/>
-      <BootstrapJs/>
     </div>
   );
 }
--- nav_bar_candidate/src/components/navbar.js
+++ nav_bar_candidate/src/components/navbar.js
@@ -3,37 +3,26 @@ import React from "react";
 function NavbarCandidate(){
     return (
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">Home</a>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Jobs</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a class="nav-link" href="#">Recruiters </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link" href="#">Resume</a>
       </li>
     </ul>
-    </div>
-    </nav>
+  </div>
+</nav>
         </div>
     )
 }
